d27488badf57eb4d786126d9f8dea9755dd57a6a
David Mudrák <david@moodle.com>
2012-06-22 16:46:10 +0200

    Improve the population of the section title
    This fixes the issue with ampersands in section summarises reported at
    https://github.com/mudrd8mz/moodle-block_course_contents/issues/6
    
    Besides that, we now use get_generic_section_name() instead of the
    hardcoded string.

--- lang/en/block_course_contents.php
+++ lang/en/block_course_contents.php
@@ -26,6 +26,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Course contents';
-$string['emptysummary'] = 'Unit {$a}';
 $string['jumptocurrenttopic'] = 'Go to current topic';
 $string['jumptocurrentweek'] = 'Go to current week';
