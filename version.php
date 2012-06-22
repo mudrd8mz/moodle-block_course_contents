c1082233e681f8491da412fd73d2191071de61b3
David Mudrák <david@moodle.com>
2012-06-22 17:51:19 +0200

    Set Moodle 2.3beta as the requirement

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'block_course_contents';
-$plugin->release    = '2.2.0';
-$plugin->version    = 2012010600;
-$plugin->requires   = 2011120500; // Moodle 2.2
-$plugin->maturity   = MATURITY_STABLE;
+$plugin->release    = 'DEV';
+$plugin->version    = 2012062200;
+$plugin->requires   = 2012061800; // Moodle 2.3beta
+$plugin->maturity   = MATURITY_ALPHA;
