723d39005d5bfef631ba1733adf931e7fdbd2478
David Mudrák <david@moodle.com>
2012-12-10 17:33:40 +0100

    Call it 2.4.0

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'block_course_contents';
-$plugin->release    = 'DEV';
+$plugin->release    = '2.4.0';
 $plugin->version    = 2012121002;
 $plugin->requires   = 2012120300; // Moodle 2.4 (Build: 20121203)
-$plugin->maturity   = MATURITY_ALPHA;
+$plugin->maturity   = MATURITY_STABLE;
