ff8bfa0565733dc6439a8298300876da337a981a
David Mudrák <david@moodle.com>
2021-02-13 15:53:42 +0100

    Implement the autotitle functionality via native PHP DOM

---
+++ classes/autotitle.php
@@ -0,0 +1,78 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace block_course_contents;
+
+/**
+ * Provides section autotitle functionality for the Course contents block
+ *
+ * @copyright  2021 David Mudrák <david@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class autotitle {
+
+    /**
+     * Extract suitable title from the HTML section summary text
+     *
+     * @param string $summary
+     * @return string
+     */
+    public static function extract_title(string $summary): string {
+
+        $summary = trim($summary);
+
+        if ($summary === '') {
+            return '';
+        }
+
+        $dom = new \DOMDocument();
+        $dom->loadHTML($summary);
+
+        return static::find_first_nonempty_text_node_value($dom);
+    }
+
+    /**
+     * Find the first non-empty text node value in the DOM tree.
+     *
+     * Recursively traverse through the DOMNode and its child nodes to seek for the
+     * first text node with non-empty value.
+     *
+     * @param \DOMNode $node
+     * @return string
+     */
+    public static function find_first_nonempty_text_node_value(\DOMNode $node): string {
+
+        if ($node->nodeType == XML_TEXT_NODE) {
+            $text = trim($node->textContent);
+
+            if ($text !== '') {
+                return $text;
+            }
+        }
+
+        $text = '';
+
+        foreach ($node->childNodes as $child) {
+            $text = static::find_first_nonempty_text_node_value($child);
+
+            if ($text !== '') {
+                break;
+            }
+        }
+
+        return $text;
+    }
+}
---
+++ tests/block_course_contents_test.php
@@ -0,0 +1,80 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace block_course_contents;
+
+/**
+ * The autotitle feature test.
+ *
+ * @package    block_course_contents
+ * @copyright  2021 David Mudrák <david@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class autotitle_test extends \advanced_testcase {
+
+    /**
+     * Test extracting suitable title from the summary HTML text.
+     *
+     * @dataProvider extract_title_data
+     * @param string $summary
+     * @param string $title
+     */
+    public function test_extract_title(string $summary, string $title) {
+        $this->assertEquals($title, autotitle::extract_title($summary));
+    }
+
+    /**
+     * Provides data for {@see self::test_extract_title()}.
+     *
+     * @return array
+     */
+    public function extract_title_data(): array {
+        return [
+            'Plain text' => [
+                'summary' => 'Welcome to this course!',
+                'title' => 'Welcome to this course!',
+            ],
+            'Heading' => [
+                'summary' => '<h3>Welcome!</h3><p>In this course, you will learn a lot.</p>',
+                'title' => 'Welcome!',
+            ],
+            'Bold' => [
+                'summary' => '<p><b>Welcome!</b> In this course, you will learn a lot.</p>',
+                'title' => 'Welcome!',
+            ],
+            'First non-empty' => [
+                'summary' => '<p class="test"><strong>  </strong><i></i></p><p></p><br /><p class="test">Hello</p>',
+                'title' => 'Hello',
+            ],
+            'Siblings' => [
+                'summary' => '<div><span>First</span><span>Two</span></div>',
+                'title' => 'First',
+            ],
+            'No actual text content, just HTML' => [
+                'summary' => "<br />\n\t<p>\n</p>",
+                'title' => '',
+            ],
+            'Empty string' => [
+                'summary' => '',
+                'title' => '',
+            ],
+            'Break lines' => [
+                'summary' => 'Hello  <br> students',
+                'title' => 'Hello',
+            ],
+        ];
+    }
+}
